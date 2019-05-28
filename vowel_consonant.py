98e8174e785a1c45f1ea82ece6ca675bae4fd4a6
Devaprakash-L <45810536+Devaprakash-L@users.noreply.github.com>
2019-05-28 20:08:07 +0530

    Update vowel_consonant.py

--- vowel_consonant.py
+++ vowel_consonant.py
@@ -1,5 +1,11 @@
 c=input()
-if c=='a' or c=='e' or c=='i' or c=='o' or c=='u':
-	print("Vowel")
+c.lower()
+if (ord(c)>=97 and ord(c)<=122): 
+	if c=='a' or c=='e' or c=='i' or c=='o' or c=='u':
+		print("Vowel")
+	else:
+		print("Consonant")
 else:
-	print("Consonant")
+	print("Invalid")
+
+	
