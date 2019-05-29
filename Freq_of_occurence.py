701e37aea36277048a3430269174e508c5e95843
Devaprakash-L <45810536+Devaprakash-L@users.noreply.github.com>
2019-05-29 11:50:05 +0530

    Create Freq_of_occurence.py

---
+++ Freq_of_occurence.py
@@ -0,0 +1,14 @@
+z=int(input())
+lis=list(map(int,input().split()))
+d={}
+for i in range(len(lis)):
+    if lis[i] not in d.keys():
+        d[lis[i]]=1
+    else:
+        d[lis[i]]+=1
+print(d)
+l1=[]
+for x,y in d.items():
+    if y!=1:
+        l1.append(x)    
+print(*sorted(l1))
