85e054fd5020784dd5792df174fc8e54febfaa97
Devaprakash-L <45810536+Devaprakash-L@users.noreply.github.com>
2019-05-30 12:30:18 +0530

    Update time-subtract.py

--- time-subtract.py
+++ time-subtract.py
@@ -1,12 +1,13 @@
 def convert(a,b):
     z=(a*60)+b
     return z
-hr=z//60
-mi=z%60
-
+    
 a,b=map(int,input().split())
 x,y=map(int,input().split())
 t1=convert(a,b)
 t2=convert(x,y)
 t=t1-t2
+hr=t//60
+mi=t%60
+
 print(hr,mi)
