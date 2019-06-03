57b9dc2048f534813f81f495dd1ae8229cc550f8
Devaprakash-L <45810536+Devaprakash-L@users.noreply.github.com>
2019-06-03 11:14:07 +0530

    Update 1st_repeat.py

--- 1st_repeat.py
+++ 1st_repeat.py
@@ -1,7 +1,13 @@
 z=int(input())
 li=list(map(int,input().split()))
+c=0
 for i in range(z):
     if li.count(li[i])!=1:
+        c+=1
         print(li[i])
         break
+if c==1:
+    pass
+else:
+    print("unique")
     
