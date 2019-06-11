5fb276fc3760ac51b0a7ab94b305f0a56a50c1b7
Devaprakash-L <45810536+Devaprakash-L@users.noreply.github.com>
2019-06-11 19:14:06 +0530

    Create subset.py

---
+++ subset.py
@@ -0,0 +1,13 @@
+a,b=map(int,input().split())
+li1=list(map(int,input().split()))
+li2=list(map(int,input().split()))
+c=0
+for i in li2:
+    if i in li1:
+        continue
+    else:
+        c=1
+if c!=1:
+    print("yes")
+else:
+    print("no")
