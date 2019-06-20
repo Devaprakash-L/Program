23d8a89ec8f36bd2433fad22bd4c9082f827132c
Devaprakash-L <45810536+Devaprakash-L@users.noreply.github.com>
2019-06-20 12:33:09 +0530

    Update maximl_substring.py

--- maximl_substring.py
+++ maximl_substring.py
@@ -1,65 +1,28 @@
-# Python 3 program to find the length 
-# of the smallest substring consisting 
-# of maximum distinct characters 
-NO_OF_CHARS = 256
+def distinct_char(st):
+    c=0
+    li=[]
+    st=list(st)
+    for i in range(len(st)):
+        if st.count(st[i])==1:
+            c+=1
+        else:
+            c=0
+            break
+    return c
 
-# Find maximum distinct characters 
-# in any string 
-def max_distinct_char(str, n): 
-
-	# Initialize all character's 
-	# count with 0 
-	count = [0] * NO_OF_CHARS 
-	
-	# Increase the count in array 
-	# if a character is found 
-	for i in range(n): 
-		count[ord(str[i])] += 1
-	
-	max_distinct = 0
-	for i in range(NO_OF_CHARS): 
-		if (count[i] != 0): 
-			max_distinct += 1	
-	
-	return max_distinct 
-
-def smallesteSubstr_maxDistictChar(str): 
-
-	n = len(str)	 # size of given string 
-
-	# Find maximum distinct characters 
-	# in any string 
-	max_distinct = max_distinct_char(str, n) 
-	minl = n	 # result 
-	
-	# Brute force approach to 
-	# find all substrings 
-	for i in range(n): 
-		for j in range(n): 
-			subs = str[i:j] 
-			subs_lenght = len(subs) 
-			sub_distinct_char = max_distinct_char(subs, 
-												subs_lenght) 
-			
-			# We have to check here both conditions together 
-			# 1. substring's distinct characters is equal 
-			# to maximum distinct characters 
-			# 2. substing's length should be minimum 
-			if (subs_lenght < minl and
-				max_distinct == sub_distinct_char): 
-				minl = subs_lenght 
-
-	return minl 
-
-# Driver Code 
-if __name__ == "__main__": 
-	
-	# Input String 
-	str = "AABBBCBB"
-	
-	l = smallesteSubstr_maxDistictChar(str); 
-	print( "The length of the smallest substring", 
-		"consisting of maximum distinct", 
-		"characters :", l) 
-
-# This code is contributed by ChitraNayal 
+def substr(st):
+    ans=0
+    n=len(st)
+    for i in range(n):
+        for j in range(n+1):
+            ele=st[i:j]
+            max=distinct_char(ele)
+            if max!=0:
+                if max>ans:
+                    ans=max
+    return ans
+    
+if __name__ == "__main__":
+    st=input()
+    print(substr(st))
+    
